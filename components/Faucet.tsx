95020667c03007e2bd276244fe66c2d3e23746aa
leon-do <leondo123@gmail.com>
2023-01-13 12:33:27 -0500

    update image and favicon

--- components/Faucet.tsx
+++ components/Faucet.tsx
@@ -39,7 +39,7 @@ export default function Faucet() {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
-            <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Testnet Faucet" />
+            <img className="mx-auto h-12 w-auto" src="logo.png" alt="Testnet Faucet" />
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Testnet Faucet</h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
