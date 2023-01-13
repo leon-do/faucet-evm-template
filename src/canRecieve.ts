2d41ee008eafbe065bbe9f2c71fbdbb019393a8b
leon-do <leondo123@gmail.com>
2023-01-13 16:37:47 -0500

    my math real gud

--- src/canRecieve.ts
+++ src/canRecieve.ts
@@ -20,9 +20,9 @@ export default async function canRecieve(address: string): Promise<CanRecieve> {
   // cooldown in seconds
   const cooldown = parseInt(process.env.COOLDOWN_HOURS as string) * 60 * 60;
   // if asked for funds after cooldown
-  if (parseInt(lastRecieve) >= now + cooldown) return { success: true, message: "🚢" };
+  if (now >= parseInt(lastRecieve) + cooldown) return { success: true, message: "🚢" };
   // calculate time left in hours
-  const timeLeft = Math.ceil((now + cooldown - parseInt(lastRecieve)) / 60 / 60);
+  const timeLeft = Math.ceil((parseInt(lastRecieve) + cooldown - now) / 60 / 60);
   return {
     success: false,
     message: `Please wait ${timeLeft} hours before requesting again`,
